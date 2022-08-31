3465a850c79cd05830b84551dc42d4f5ac75b88f
Yonghai <dylanyonghaih@gmail.com>
2022-08-31 18:45:48 -0400

    create game grid

--- script.js
+++ script.js
@@ -19,6 +19,40 @@ const controlBar = {
   // height is equal to the size of one cell (which is 100)
   height: cellSize,
 };
+// use class to create models for similar objects
+class Cell {
+  constructor(x, y) {
+    //set the cell coordinate to equal to the position
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+  }
+
+  draw() {
+    // use stroke(border) to draw black rectangle for the cells
+    ctx.strokeStyle = "Black";
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
+}
+// now we need to draw all the cells on the board with a function
+function createGrid() {
+  // y starts at below the top bar, which is cell size
+  for (let y = cellSize; y < canvas.height; y += cellSize) {
+    // x starts at 0
+    for (let x = 0; x < canvas.width; x += cellSize) {
+      // push the cell to the array of game grid, where x,y is the position of the cell
+      gameGrid.push(new Cell(x, y));
+    }
+  }
+}
+createGrid();
+// after creating the game grid, now use a function and loop to draw the grid
+function handleGameGrid() {
+  for (let i = 0; i < gameGrid.length; i++) {
+    gameGrid[i].draw();
+  }
+}
 // projectiles
 // defenders
 // enemies
@@ -28,8 +62,10 @@ const controlBar = {
 function animate() {
   // call the draw function to draw blue
   ctx.fillStyle = "blue";
-  // built in draw function to draw rectangle 0,0 is the start position (x,y) and the other variables are the end
+  // built in draw function to draw rectangle 0,0 is the start position (x,y) and the other variables are the dimension
   ctx.fillRect(0, 0, controlBar.width, controlBar.height);
+  // draw out the grid
+  handleGameGrid();
   // built in animate function to run animation (recursion to run again and again)
   requestAnimationFrame(animate);
 }
